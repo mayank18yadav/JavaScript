3c8b1866d493f290ff9631449c676743ea8ebe50
Mayank Yadav <136826867+mayank18yadav@users.noreply.github.com>
2024-02-01 06:15:51 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -50,3 +50,26 @@ const myFunction = function(){
 // objects --> object
 // Functions --> object function
 
+//  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap(Non-Primitive)
+
+let myYoutubename = "mayankydvdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "mayank@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
