c282071555d35b2b2ef9492a3e3ac694cf87e3a3
Mayank Yadav <136826867+mayank18yadav@users.noreply.github.com>
2024-02-22 09:13:00 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+// while loop
+
+ let index = 2
+// while (index <= 12) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+let myArray = ['Mayank', 'Manjeet', 'Viraj']
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
